d7240f5089b18d317e9d95ba6452e64cc24fd63b
MG <maciej.gusta@gmail.com>
2024-09-16 08:27:34 +0200

    something

---
+++ pies/TestPies.java
@@ -0,0 +1,10 @@
+class TestPies {
+    public static void main(String[] args){
+        Pies maniek = new Pies();
+        maniek.imie = "Maniek";
+        maniek.rasa = "Shih thu";
+        maniek.wiek = 4;
+
+        System.out.println(maniek.imie + maniek.rasa + maniek.wiek);
+    }
+}
